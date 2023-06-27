bab12ad0348c1284f392f915806572f91662d11b
yuda <saevyeokvyeol@gmail.com>
2023-06-27 18:52:19 +0900

    230627

---
+++ python-algorithm-interview/2-6.StringManipulation/3.reorder-data-in-log-files.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+
+    # 람다와 + 연산자를 이용한 풀이
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        
+        # 2개의 키를 람다 표현식으로 정렬
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
