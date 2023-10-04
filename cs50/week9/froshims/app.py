c307db02ef49e550054d052537bc7c56eeb3f21d
YUDA <saevyeokvyeol@gmail.com>
2023-10-04 19:12:21 +0900

    231004 cs50 week9

---
+++ cs50/week9/froshims/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+REGISTRANTS = {}
+
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbee"
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    if not name:
+        return render_template("failure.html")
+    if sport not in SPORTS:
+        return render_template("failure.html")
+    REGISTRANTS[name] = sport
+    return render_template("success.html")
+
+@app.route("/registrants")
+def registerants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
